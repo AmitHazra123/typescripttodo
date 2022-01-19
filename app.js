d29d3609b1a02a75c29e8b2063609286b7d8bd09
Amit Hazra <amithazra@Amits-MacBook-Air.local>
2022-01-20 00:00:36 +0530

    feat: chapter_7

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-var container = document.getElementById('container');
-function Counter(el) {
-    var _this = this;
-    this.count = 0;
-    el.innerHTML = this.count;
-    el.addEventListener('click', function () {
-        _this.count += 1;
-        el.innerHTML = _this.count;
-    });
+var _a;
+var array = [123, "Pick up drycleaning", false];
+var id = array[0], title = array[1], completed = array[2];
+var a = 1;
+var b = 5;
+_a = [b, a], a = _a[0], b = _a[1];
+function getTodo(id) {
+    var todo = {
+        id1: 123,
+        title1: "Pick up drycleaning",
+        completed1: false
+    };
+    return todo;
 }
-new Counter(container);
+var _b = getTodo(123), id1 = _b.id1, title1 = _b.title1, completed1 = _b.completed1;
+function countdown(_a) {
+    var initial = _a.initial, _b = _a.final, final = _b === void 0 ? 0 : _b, _c = _a.interval, interval = _c === void 0 ? 1 : _c, current = _a.initial;
+    while (current > final) {
+        console.log(current);
+        current -= interval;
+    }
+}
--- app.ts
+++ app.ts
@@ -1,17 +1,30 @@
-var container = document.getElementById('container');
+var array = [123, "Pick up drycleaning", false];
+var [id, title, completed] = array;
 
-function Counter(el) {
-    
-    this.count = 0;
+var a = 1;
+var b = 5;
 
-    el.innerHTML = this.count;
+[a, b] = [b, a];
 
-    el.addEventListener('click',
-        () => {
-            this.count += 1;
-            el.innerHTML = this.count;
-        }
-    );
+function getTodo(id) {
+    var todo = {
+        id1: 123,
+        title1: "Pick up drycleaning",
+        completed1: false
+    }
+    return todo;
 }
 
-new Counter(container);
+var { id1, title1, completed1 } = getTodo(123);
+
+function countdown({
+    initial,
+    final: final = 0,
+    interval: interval = 1,
+    initial: current
+}) {
+    while (current > final) {
+        console.log(current);
+        current -= interval;
+    }
+}
