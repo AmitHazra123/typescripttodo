648a27fb003f82af8b273f8dfcd72cb0bc9533e6
Amit Hazra <amithazra@Amits-MacBook-Air.local>
2022-01-20 00:25:04 +0530

    feat: chapter_9

--- app.js
+++ app.js
@@ -1,34 +1,10 @@
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
-    }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
-function calculate(action) {
-    var values = [];
-    for (var _i = 1; _i < arguments.length; _i++) {
-        values[_i - 1] = arguments[_i];
-    }
-    var total = 0;
-    for (var _a = 0, values_1 = values; _a < values_1.length; _a++) {
-        var value = values_1[_a];
-        switch (action) {
-            case 'add':
-                total += value;
-                break;
-            case 'subtract':
-                total -= value;
-                break;
-        }
-    }
-    return total;
+var _a;
+var osPrefix = 'os_';
+var support = (_a = {},
+    _a[osPrefix + 'Windows'] = isSupported('Windows'),
+    _a[osPrefix + 'iOS'] = isSupported('iOS'),
+    _a[osPrefix + 'Android'] = isSupported('Android'),
+    _a);
+function isSupported(os) {
+    return Math.random() >= 0.5;
 }
-calculate('subtract', 1, 2, 3, 4, 5);
-var source = [3, 4, 5];
-var target = __spreadArray(__spreadArray([1, 2], source, true), [6, 7], false);
-var list = [1, 2, 3];
-var toAdd = [4, 5, 6];
-list.push.apply(list, toAdd);
--- app.ts
+++ app.ts
@@ -1,26 +1,11 @@
-function calculate (action, ...values) {
-    var total = 0;
+const osPrefix = 'os_';
 
-    for (var value of values) {
-        switch(action) {
-            case 'add':
-                total += value;
-                break;
-            case 'subtract':
-                total -= value;
-                break;
-        }
-    }
-
-    return total;
+var support = {
+    [osPrefix + 'Windows']: isSupported('Windows'),
+    [osPrefix + 'iOS']: isSupported('iOS'),
+    [osPrefix + 'Android']: isSupported('Android')
 }
 
-calculate('subtract', 1, 2, 3, 4, 5);
-
-var source = [3, 4, 5];
-var target = [1, 2, ...source, 6, 7];
-
-var list = [1, 2, 3];
-var toAdd = [4, 5, 6];
-
-list.push(...toAdd);
+function isSupported(os) {
+    return Math.random() >= 0.5;
+}
