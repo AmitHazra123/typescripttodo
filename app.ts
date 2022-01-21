db06453ecd3f19a2627cd24f41ff48cab7eccdcb
Amit Hazra <amithazra@Amits-MacBook-Air.local>
2022-01-21 16:13:19 +0530

    feat: chapter_29

--- app.ts
+++ app.ts
@@ -1,15 +1,30 @@
-function clone<T>(value: T): T {
-    let serialized = JSON.stringify(value);
-    return JSON.parse(serialized);
-}
-clone('Hello!');
-clone(123);
+var array: number[] = [1, 2, 3];
+var array2: Array<number> = [1, 2, 3];
 
-var todo: Todo = {
-    id: 1,
-    name: 'Pick up drycleaning',
-    state: TodoState.Active
+class KeyValuePair<TKey, TValue> {
+    constructor(
+        public key: TKey,
+        public value: TValue
+    ) {
+
+    }
 }
 
-clone(todo);
-clone({name: 'Jess'});
+let pair1 = new KeyValuePair<number, string>(1, 'First');
+let pair2 = new KeyValuePair<string, Date>('Second', new Date(Date.now()));
+let pair3 = new KeyValuePair<number, string>(3, 'Third');
+
+class KeyValuePirPrinter<T, U> {
+    constructor(private pairs: KeyValuePair<T, U>[]){
+
+    }
+
+    print() {
+        for (let p of this.pairs) {
+            console.log(`${p.key}: ${p.value}`);
+        }
+    }
+}
+
+var printer = new KeyValuePirPrinter([pair1, pair3]);
+printer.print();
