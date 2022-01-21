728c9ddedec272d50c6436f7b09af87a7c008899
Amit Hazra <amithazra@Amits-MacBook-Air.local>
2022-01-21 16:02:02 +0530

    feat: chapter_28

--- app.js
+++ app.js
@@ -1,94 +1,13 @@
-/*
-
-function TodoService() {
-    this.todos = [];
+function clone(value) {
+    var serialized = JSON.stringify(value);
+    return JSON.parse(serialized);
 }
-
-TodoService.prototype.getAll = function() {
-    return this.todos;
-}
-
-*/
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-// var todo: Todo = {
-//     name: "Pick up drycleaning",
-//     get state() {
-//         return this._state;
-//     },
-//     set state(newState) {
-//         if(newState == TodoState.Complete) {
-//             var canBeCompleted =
-//                 this.state == TodoState.Active
-//                 || this.state == TodoState.Deleted;
-//             if(!canBeCompleted) {
-//                 throw "Todo must be Active or Deleted in order to be marked Completed";
-//             }
-//         }
-//         this._state = newState;
-//     }
-// }
-var SmartTodo = /** @class */ (function () {
-    function SmartTodo(name) {
-        this.name = name;
-    }
-    Object.defineProperty(SmartTodo.prototype, "state", {
-        get: function () {
-            return this._state;
-        },
-        set: function (newState) {
-            if (newState == TodoState.Complete) {
-                var canBeCompleted = this.state == TodoState.Active
-                    || this.state == TodoState.Deleted;
-                if (!canBeCompleted) {
-                    throw "Todo must be Active or Deleted in order to be marked Completed";
-                }
-            }
-            this._state = newState;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    return SmartTodo;
-}());
-var todo = new SmartTodo("Pick up drycleaning");
-todo.state = TodoState.Complete;
-todo.state;
-var TodoStateChanger = /** @class */ (function () {
-    function TodoStateChanger(newState) {
-        this.newState = newState;
-    }
-    TodoStateChanger.prototype.changeState = function (todo) {
-        if (this.canChangeState(todo)) {
-            todo.state = this.newState;
-        }
-        return todo;
-    };
-    return TodoStateChanger;
-}());
-var CompleteTodoStateChanger = /** @class */ (function (_super) {
-    __extends(CompleteTodoStateChanger, _super);
-    function CompleteTodoStateChanger() {
-        var _this = _super.call(this, TodoState.Complete) || this;
-        _this.newState;
-        return _this;
-    }
-    CompleteTodoStateChanger.prototype.canChangeState = function (todo) {
-        return !!todo && (todo.state == TodoState.Active
-            || todo.state == TodoState.Deleted);
-    };
-    return CompleteTodoStateChanger;
-}(TodoStateChanger));
+clone('Hello!');
+clone(123);
+var todo = {
+    id: 1,
+    name: 'Pick up drycleaning',
+    state: TodoState.Active
+};
+clone(todo);
+clone({ name: 'Jess' });
--- app.ts
+++ app.ts
@@ -1,137 +1,15 @@
-/*
+function clone<T>(value: T): T {
+    let serialized = JSON.stringify(value);
+    return JSON.parse(serialized);
+}
+clone('Hello!');
+clone(123);
 
-function TodoService() {
-    this.todos = [];
+var todo: Todo = {
+    id: 1,
+    name: 'Pick up drycleaning',
+    state: TodoState.Active
 }
 
-TodoService.prototype.getAll = function() {
-    return this.todos;
-}
-
-*/
-
-// class TodoService {
-
-//     private static lastId: number = 0;
-
-//     private get nextId() {
-//         return TodoService.getNextId();
-//     }
-
-//     private set nextId(nextId) {
-//         TodoService.lastId = nextId - 1;
-//     }
-
-//     constructor(private todos: Todo[]) {
-//     }
-
-//     add(todo: Todo) {
-//         var newId = TodoService.getNextId();
-//     }
-
-//     private getAll() {
-//         return this.todos;
-//     }
-
-//     static getNextId() {
-//         return TodoService.lastId += 1;
-//     }
-// }
-
-// enum TodoState {
-//     New = 1,
-//     Active,
-//     Complete,
-//     Deleted
-// }
-
-interface Todo {
-    name: string,
-    state: TodoState
-}
-
-// var todo: Todo = {
-//     name: "Pick up drycleaning",
-//     get state() {
-//         return this._state;
-//     },
-//     set state(newState) {
-        
-//         if(newState == TodoState.Complete) {
-            
-//             var canBeCompleted =
-//                 this.state == TodoState.Active
-//                 || this.state == TodoState.Deleted;
-            
-//             if(!canBeCompleted) {
-//                 throw "Todo must be Active or Deleted in order to be marked Completed";
-//             }
-
-//         }
-
-//         this._state = newState;
-//     }
-// }
-
-class SmartTodo {
-    _state: TodoState
-
-    get state() {
-        return this._state;
-    }
-
-    set state(newState) {
-        
-        if(newState == TodoState.Complete) {
-            
-            var canBeCompleted =
-                this.state == TodoState.Active
-                || this.state == TodoState.Deleted;
-            
-            if(!canBeCompleted) {
-                throw "Todo must be Active or Deleted in order to be marked Completed";
-            }
-
-        }
-
-        this._state = newState;
-    }
-
-    constructor(public name: string) {
-    }
-}
-
-var todo = new SmartTodo("Pick up drycleaning");
-
-todo.state = TodoState.Complete;
-
-todo.state;
-
-abstract class TodoStateChanger {
-    constructor(protected newState: TodoState) {
-    }
-
-    abstract canChangeState(todo: Todo): boolean;
-
-    changeState(todo: Todo): Todo {
-        if(this.canChangeState(todo)) {
-            todo.state = this.newState;
-        }
-
-        return todo;
-    }
-}
-
-class CompleteTodoStateChanger extends TodoStateChanger {
-    constructor() {
-        super(TodoState.Complete);
-        this.newState;
-    }
-
-    canChangeState(todo: Todo): boolean {
-        return !!todo && (
-            todo.state == TodoState.Active
-            || todo.state == TodoState.Deleted
-        );
-    }
-}
+clone(todo);
+clone({name: 'Jess'});
