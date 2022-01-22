ae377611d054a4e9266a204a567243404e7c5fd4
Amit Hazra <amithazra@Amits-MacBook-Air.local>
2022-01-22 13:24:43 +0530

    feat: chapter_31

--- model.ts
+++ model.ts
@@ -1,19 +1,34 @@
-interface ITodoService {
-    add(todo: Todo): Todo;
-    delete(todoId: number): void;
-    getAll(): Todo[];
-    getById(todoId: number): Todo;
+namespace TodoApp.Model {
+
+    export interface Todo {
+        id: number;
+        name: string;
+        state: TodoState;
+    }
+
 }
 
-interface Todo {
-    id: number;
-    name: string;
-    state: TodoState;
+namespace TodoApp.Model {
+
+    export enum TodoState {
+        New = 1,
+        Active,
+        Complete,
+        Deleted
+    }
+
 }
 
-enum TodoState {
-    New = 1,
-    Active,
-    Complete,
-    Deleted
+namespace DataAcess {
+
+    import Model = TodoApp.Model;
+    import Todo = Model.Todo;
+
+    export interface ITodoService {
+        add(todo: Todo): Todo;
+        delete(todoId: number): void;
+        getAll(): Todo[];
+        getById(todoId: number): Todo;
+    }
+
 }
